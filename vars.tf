63e85077681ab5b0ed0d8e72694e712e3d65cbe1
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-08 11:35:07 +0530

    updating

--- vars.tf
+++ vars.tf
@@ -6,16 +6,16 @@ variable "RDS_MYSQL_STORAGE" {}
 variable "RDS_MYSQL_ENGINE_VERSION" {}
 variable "RDS_INSTANCE_TYPE" {}
 
-# DocDB 
-variable "DOCDB_PORT" {}
-variable "DOCDB_INSTANCE_CLASS" {}
-variable "DOCDB_INSTANCE_COUNT" {}
+# # DocDB 
+# variable "DOCDB_PORT" {}
+# variable "DOCDB_INSTANCE_CLASS" {}
+# variable "DOCDB_INSTANCE_COUNT" {}
 
-# Elastic Cache
-variable "REDIS_PORT" {}
-variable "REDIS_INSTANCE_NODETYPE" {}
-variable "REDIS_INSTANCE_COUNT" {}
-variable "REDIS_ENGINE_VERSION" {}
+# # Elastic Cache
+# variable "REDIS_PORT" {}
+# variable "REDIS_INSTANCE_NODETYPE" {}
+# variable "REDIS_INSTANCE_COUNT" {}
+# variable "REDIS_ENGINE_VERSION" {}
 
 # RabbitMQ
 
